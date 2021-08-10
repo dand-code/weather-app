ded95f3dcf4ab0b33116712376f5b69197306142
Dandara Almeida <dandara.avazquez@gmail.com>
2021-08-10 13:53:23 +0200

    set worker servise to add web to home screen

---
+++ docs/info.js
@@ -0,0 +1,10 @@
+let info = [
+    {
+      slug: 'Now Weather',
+      name: 'Now Weather App',
+      author: 'Dandara Almeida',
+      twitter: '@dand_code',
+      github: 'https://github.com/dand-code'
+    }
+];
+  
---
+++ docs/sw.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-restricted-globals */
+self.importScripts("info.js");
+
+const cacheName = 'appNowWeather';
+const appShellFiles = [
+    '/weather-app/public/',
+    '/weather-app/public/index.html',
+    '/weather-app/public/info.js',
+    '/weather-app/public/favicon.ico',
+    '/weather-app/public/logo192.png',
+    '/weather-app/public/logo512.png',
+];
+
+const contentToCache = appShellFiles;
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+
+//Fetching content using Service Worker
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
