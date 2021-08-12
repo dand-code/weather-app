00b9e60a798d6ac1275dd791c11bcc6b2a16e827
Dandara Almeida <dandara.avazquez@gmail.com>
2021-08-12 12:41:47 +0200

    replace service worker from index.html to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,19 @@ const [weather, setWeather] = useState(false);
   
 //service worker  
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker.register("/public/sw.js").then(() => { console.log('Service Worker Registered'); });
-// };
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      console.log('Worker registration successful', registration.scope);
+    }, function(err) {
+      console.log('Worker registration failed', err);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  });
+} else {
+  console.log('Service Worker is not supported by browser.');
+}
   
 /* HANDLE DATA: */
 
